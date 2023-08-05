69c6db22ef8a00086711579875b8b83b8eff0b35
JAYAKUMARRAMAR <jayakumarramar92@gmail.com>
2023-08-05 20:27:47 +0530

    Step:1:Create Parent Class(RegLogIn)

---
+++ src/main/java/com/fullstack/restapi/RegLogIn/RegLogInApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.restapi.RegLogIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegLogInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegLogInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullstack/restapi/RegLogIn/RegLogInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.restapi.RegLogIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegLogInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
